e99e5cddb66539c90b4c334072b4350ec11874cc
Jasper Yeh <jacyeh@ucsc.edu>
2019-07-08 22:32:06 -0700

    arm rotates with mouse and controller inputs

--- Platformer2D/Assets/Scripts/Pivot.cs
+++ Platformer2D/Assets/Scripts/Pivot.cs
@@ -5,26 +5,71 @@ using UnityEngine;
 public class Pivot : MonoBehaviour
 {
     public GameObject player;
+    private bool isMouse = true;
 
     private void FixedUpdate()
     {
-        Vector3 distance = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
 
-        distance.Normalize();
-
-        float rotationZ = Mathf.Atan2(distance.y, distance.x) * Mathf.Rad2Deg;
-
-        transform.rotation = Quaternion.Euler(0f, 0f, rotationZ);
-
-        if (rotationZ < -90 || rotationZ > 90)
+        if (Input.GetAxis("Mouse X") < 0 || Input.GetAxis("Mouse X") > 0 || Input.GetAxis("Mouse Y") < 0 || Input.GetAxis("Mouse Y") > 0)
         {
-            if(player.transform.eulerAngles.y == 0)
+            isMouse = true;
+        }
+        else if (Input.GetAxis("Horizontal") < 0 || Input.GetAxis("Horizontal") > 0 || Input.GetAxis("Vertical") < 0 || Input.GetAxis("Vertical") > 0)
+        {
+            isMouse = false;
+        }
+        
+
+        if (isMouse)
+        {
+            Debug.Log("MOUSE INPUT");
+
+            Vector3 distance = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+            distance.Normalize();
+
+            float rotationZ = Mathf.Atan2(distance.y, distance.x) * Mathf.Rad2Deg;
+
+            transform.rotation = Quaternion.Euler(0f, 0f, rotationZ);
+
+            if (rotationZ < -90 || rotationZ > 90)
             {
-                transform.localRotation = Quaternion.Euler(180, 0, -rotationZ);
-            } else if (player.transform.eulerAngles.y == 180)
-            {
-                transform.localRotation = Quaternion.Euler(180, 180, -rotationZ);
+                if (player.transform.eulerAngles.y == 0)
+                {
+                    transform.localRotation = Quaternion.Euler(180, 0, -rotationZ);
+                }
+                else if (player.transform.eulerAngles.y == 180)
+                {
+                    transform.localRotation = Quaternion.Euler(180, 180, -rotationZ);
+                }
             }
         }
+
+        if (!isMouse)
+        {
+            Debug.Log("CONTROLLER INPUT");
+            var joystickX = Input.GetAxis("Horizontal");
+            var joystickY = Input.GetAxis("Vertical");
+
+            float rotationZ = Mathf.Atan2(joystickY, joystickX) * Mathf.Rad2Deg;
+
+            transform.rotation = Quaternion.Euler(0f, 0f, rotationZ);
+
+            if (rotationZ < -90 || rotationZ > 90)
+            {
+                if (player.transform.eulerAngles.y == 0)
+                {
+                    transform.localRotation = Quaternion.Euler(180, 0, -rotationZ);
+                }
+                else if (player.transform.eulerAngles.y == 180)
+                {
+                    transform.localRotation = Quaternion.Euler(180, 180, -rotationZ);
+                }
+            }
+        }
+
     }
+
+
+
+
 }
