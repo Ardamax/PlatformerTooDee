b9184eb3b5422ba224e33ee44921d4a9a97df58c
Jasper Yeh <jacyeh@ucsc.edu>
2019-07-09 19:35:14 -0700

    sorting layers

---
+++ Platformer2D/Assets/Scripts/Shoot.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour
+{
+
+    public GameObject bullet;
+    public Transform bulletSpawn;
+    private float cooldown;
+    public float fireRate;
+
+    private void FixedUpdate()
+    {
+        bool shoot = Input.GetButton("Fire1");
+
+        if (cooldown <= 0)
+        {
+            if (shoot)
+            {
+                Instantiate(bullet, bulletSpawn.position, bulletSpawn.rotation);
+                cooldown = fireRate;
+            }
+        } else
+        {
+            cooldown -= Time.deltaTime;
+        }
+
+    }
+}
